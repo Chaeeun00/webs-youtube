b229ad9f9a4463a9ad4cfadba76296d4b7364242
chea <ypd06220@naver.com>
2024-04-14 18:59:55 +0900

    메인 콘텐츠 작업

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import Main from '../components/section/Main'
+import Today from '../components/contents/Today'
+import Developer from '../components/contents/Developer'
+import Webd from '../components/contents/Webd'
+import Website from '../components/contents/Website'
+import Gsap from '../components/contents/Gsap'
+import Portfolio from '../components/contents/Portfolio'
+import Youtube from '../components/contents/Youtube'
 
 const Home = () => {
   return (
     <Main
       title = "송채은 유튜브🐰"
       description = "송채은 유튜브에 오신 것을 환영합니다.">
-      Home
+      
+      <Today />
+      <Developer />
+      <Webd />
+      <Website />
+      <Gsap />
+      <Portfolio />
+      <Youtube />
     </Main>
   )
 }
