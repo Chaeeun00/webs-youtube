4ef470bb365f18e06e601796a06dceb630ab957e
chea <ypd06220@naver.com>
2024-08-23 14:08:17 +0900

    마무리

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+// 서버의 기본 URL 정의
 export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
@@ -7,13 +8,19 @@ const options = {
         maxResults: 48,
     },
     headers: {
-        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,  // 환경 변수에서 API 키를 가져옵니다.
         'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
     },
 };
 
+// API 요청 함수 정의
 export const fetchFromAPI = async (url) => {
-    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
-    console.log(data)
-    return data;
+    try {
+        const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.error('API call failed:', error.message);
+        return null;
+    }
 };
