4654d5a5cce1799935791ffa96ff96364d1bdb32
chea <ypd06220@naver.com>
2024-08-27 14:53:39 +0900

    마무리

--- src/components/section/Header.jsx
+++ src/components/section/Header.jsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 import Logo from '../header/Logo';
 import Menu from '../header/Menu';
 import Sns from '../header/Sns';
 
 const Header = () => {
-    const [ isMenuActive, setIsMenuActive ] = useState(false);
+    const [isMenuActive, setIsMenuActive] = useState(false);
 
     const toggleMenu = () => {
         setIsMenuActive(!isMenuActive);
-    }
+    };
 
     return (
         <header id='header' role='banner' className={isMenuActive ? 'active' : ''}>
@@ -17,7 +17,7 @@ const Header = () => {
             <Menu />
             <Sns />
         </header>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
