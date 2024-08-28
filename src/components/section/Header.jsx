24e53c92497d12a057b741b3c57953a6242d017d
chea <ypd06220@naver.com>
2024-08-28 14:10:19 +0900

    마무리

--- src/components/section/Header.jsx
+++ src/components/section/Header.jsx
@@ -20,19 +20,4 @@ const Header = () => {
     );
 };
 
-window.addEventListener('resize', () => {
-    const logo = document.querySelector('.header__logo');
-    if (window.innerHeight < 600) {
-        // 브라우저가 주소창 때문에 축소된 경우
-        logo.style.top = '20px'; // 원하는 값으로 조정
-    } else {
-        logo.style.top = '10px';
-    }
-});
-window.addEventListener('scroll', () => {
-    const logo = document.querySelector('.header__logo');
-    logo.style.top = window.scrollY + 10 + 'px'; // 스크롤에 따른 위치 조정
-});
-
-
 export default Header;
