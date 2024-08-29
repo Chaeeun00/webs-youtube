d184edfc873850abdf5248bdb00fc2b0ece24dd5
chea <ypd06220@naver.com>
2024-08-30 00:50:59 +0900

    마무리

--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Logo = ({ toggleMenu }) => {
+const Logo = ({ toggleMenu, isMenuActive }) => {
     return (
-        <h1 className='header__logo'>
+        <h1 className={`header__logo ${isMenuActive ? 'menu-active' : ''}`}>
             <Link to='/'>
                 <em aria-hidden='true' onClick={toggleMenu}></em>
                 <span>songs<br />youtube</span>
